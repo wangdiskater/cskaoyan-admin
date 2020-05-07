6c47983361e8b181577cf0c208da70af7af0da84
wangdi <996057150@qq.com>
2020-05-08 00:03:19 +0800

    update  user

--- src/views/user/manage.vue
+++ src/views/user/manage.vue
@@ -3,8 +3,12 @@
 
     <!-- 查询和其他操作 -->
     <div class="filter-container">
+      <el-button v-permission="['POST /admin/category/create']" class="filter-item" type="primary" icon="el-icon-edit" @click="handleCreate">添加</el-button>
       <el-input v-model="listQuery.username" clearable class="filter-item" style="width: 200px;" placeholder="请输入用户名"/>
-      <el-input v-model="listQuery.mobile" clearable class="filter-item" style="width: 200px;" placeholder="请输入手机号"/>
+      <el-select v-model="listQuery.orderStatusArray" multiple style="width: 200px" class="filter-item" placeholder="请选择角色">
+        <el-option v-for="(key, value) in statusMap" :key="key" :label="key" :value="value"/>
+      </el-select>
+      <!--<el-input v-model="listQuery.mobile" clearable class="filter-item" style="width: 200px;" placeholder="请输入手机号"/>-->
       <el-button class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">查找</el-button>
       <el-button :loading="downloadLoading" class="filter-item" type="primary" icon="el-icon-download" @click="handleDownload">导出</el-button>
     </div>
@@ -41,12 +45,34 @@
 
     <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
 
+    <!-- 添加用戶 -->
+    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
+      <el-form ref="dataForm" :rules="rules" :model="dataForm" status-icon label-position="left" label-width="100px" style="width: 400px; margin-left:50px;">
+        <el-form-item label="用戶賬戶" prop="username">
+          <el-input v-model="dataForm.username"/>
+        </el-form-item>
+        <el-form-item label="用户密码" prop="password">
+          <el-input v-model="dataForm.password"/>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取消</el-button>
+        <el-button v-if="dialogStatus=='create'" type="primary" @click="createData">确定</el-button>
+      </div>
+    </el-dialog>
+
   </div>
 </template>
 
 <script>
 import { fetchList } from '@/api/user'
 import Pagination from '@/components/Pagination' // Secondary package based on el-pagination
+
+const statusMap = {
+  101: '开发',
+  102: '测试',
+  103: '运维'
+}
 
 export default {
   name: 'User',
@@ -60,14 +86,28 @@ export default {
         page: 1,
         limit: 20,
         username: undefined,
-        mobile: undefined,
+        orderStatusArray: [],
         sort: 'add_time',
         order: 'desc'
       },
+      statusMap,
       downloadLoading: false,
       genderDic: ['未知', '男', '女'],
       levelDic: ['普通用户', 'VIP用户', '高级VIP用户'],
-      statusDic: ['可用', '禁用', '注销']
+      statusDic: ['可用', '禁用', '注销'],
+      dataForm: {
+        username: '',
+        password: ''
+      },
+      dialogFormVisible: false,
+      dialogStatus: '',
+      textMap: {
+        update: '编辑',
+        create: '创建'
+      },
+      rules: {
+        name: [{ required: true, message: '类目名不能为空', trigger: 'blur' }]
+      }
     }
   },
   created() {
@@ -97,6 +137,49 @@ export default {
         const filterVal = ['username', 'mobile', 'gender', 'birthday', 'status']
         excel.export_json_to_excel2(tHeader, this.list, filterVal, '用户信息')
         this.downloadLoading = false
+      })
+    },
+    handleCreate() {
+      this.resetForm()
+      this.dialogStatus = 'create'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    resetForm() {
+      this.dataForm = {
+        id: undefined,
+        name: '',
+        keywords: '',
+        level: 'L2',
+        pid: 0,
+        desc: '',
+        iconUrl: '',
+        picUrl: ''
+      }
+    },
+    createData() {
+      this.$refs['dataForm'].validate(valid => {
+        if (valid) {
+          createCategory(this.dataForm)
+            .then(response => {
+              this.getList()
+              // 更新L1目录
+              this.getCatL1()
+              this.dialogFormVisible = false
+              this.$notify.success({
+                title: '成功',
+                message: '创建成功'
+              })
+            })
+            .catch(response => {
+              this.$notify.error({
+                title: '失败',
+                message: response.data.errmsg
+              })
+            })
+        }
       })
     }
   }
